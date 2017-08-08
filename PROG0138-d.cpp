d68207afc3071e3e8fd3e05e12e8b97069a5cd26
Deepak Aggarwal <daggarwal77@gmail.com>
2017-08-08 16:50:11 +0530

    Birthday Paradox

---
+++ PROG0138-d.cpp
@@ -0,0 +1,30 @@
+//Birthday paradox
+#include <bits/stdc++.h>
+using namespace std;
+
+const double days = 365;
+
+void prob(){
+	double pDiff = 1;
+	int fav = 364;
+
+	for(int i = 2; i < 5; ++i){
+		pDiff *= fav / days;
+		--fav;
+	}
+
+	//5 to 75
+	for(int i = 5; i <= 75; ++i){
+		pDiff *= fav / days;
+		--fav;
+
+		if (i % 5 == 0){
+			printf("%2d %13.12f\n", i, 1 - pDiff);
+		}
+
+	}
+}
+
+int main(){
+	prob();
+}
